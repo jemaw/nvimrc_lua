651eb36438afa33bef28db3d244aa66803dde77a
Jean Wanka <jm.wanka@gmail.com>
2022-01-13 18:40:35 +0100

    add snippets

--- lua/plugins/cmp_conf.lua
+++ lua/plugins/cmp_conf.lua
@@ -1,26 +1,51 @@
-local opt = vim.opt  -- to set options
-local cmp = require'cmp'
-opt.completeopt = "menu,menuone,noselect"
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+require("luasnip.loaders.from_vscode").load()
+local cmp = require("cmp")
+
+vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
     sources = cmp.config.sources({
         { name = "buffer" },
         { name = "nvim_lsp" },
-        --{ name = "luasnip" },
+        { name = "luasnip" },
         { name = "path" }
     }),
 
     mapping = {
         ["<cr>"] = cmp.mapping.confirm({select = true}),
-        ["<s-tab>"] = cmp.mapping.select_prev_item(),
-        ["<tab>"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" })
     },
 
-    -- snippet = {
-      -- expand = function(args)
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- end,
-    -- }
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    }
 })
 
 -- Use buffer source for `/` search (if you enabled `native_menu`, this won't work anymore).
--- lua/plugins/paq.lua
+++ lua/plugins/paq.lua
@@ -37,7 +37,12 @@ require "paq" {
     "hrsh7th/cmp-cmdline";
     "hrsh7th/nvim-cmp";
     "L3MON4D3/LuaSnip";
-    "saadparwaiz1/cmp_luasnip"
+    "saadparwaiz1/cmp_luasnip";
+    -- snippets
+    "L3MON4D3/LuaSnip";
+    "saadparwaiz1/cmp_luasnip";
+    "rafamadriz/friendly-snippets"
+
 }
 
 require('gitsigns').setup()
